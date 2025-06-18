c0d1daa3f70245eaf47000cfe1b6a9d0c6341f60
ZoeNereaVicente <zvicente@uade.edu.ar>
2025-06-17 21:56:25 -0300

    Commit final

---
+++ Pagina/boton.js
@@ -0,0 +1,7 @@
+// botón "que comience la magia"
+const btnMagia = document.getElementById('btn-magia');
+if (btnMagia) {
+  btnMagia.addEventListener('click', function () {
+    window.location.href = 'Tickets.html';
+  });
+}
--- Pagina/index.js
+++ Pagina/index.js
@@ -19,3 +19,20 @@ function toggleCarrito() {
   const panel = document.getElementById("carrito-panel");
   panel.classList.toggle("oculto");
 }
+
+const btnBuscar = document.getElementById('buscar-precios');
+  if (btnBuscar) {
+    btnBuscar.addEventListener('click', () => {
+      const adultos = parseInt(document.getElementById('adultos').value);
+      const ninos = parseInt(document.getElementById('ninos').value);
+      if (isNaN(adultos) || adultos < 1 || adultos > 8) {
+        alert("Seleccioná entre 1 y 8 adultos.");
+        return;
+      }
+      if (isNaN(ninos) || ninos < 1 || ninos > 10) {
+        alert("Seleccioná entre 1 y 10 menores.");
+        return;
+      }
+      window.location.href = "Tickets.html";
+    });
+  }
